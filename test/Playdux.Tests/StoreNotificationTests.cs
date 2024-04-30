725f1f56765f3b721bea627acd6e6941770fe7f0
Cole Schultz <schultzcole@gmail.com>
2024-04-29 22:50:39 -0500

    Move ObservableFor error test into StoreNotificationTests.cs

--- test/Playdux.Tests/StoreNotificationTests.cs
+++ test/Playdux.Tests/StoreNotificationTests.cs
@@ -8,59 +8,82 @@ public class StoreNotificationTests
     [Fact]
     public void ObserverNotNotifiedOnDispatchWhenReducerDoesNotChangeState()
     {
-            SimpleTestState init = new(42);
-            var simpleStore = new Store<SimpleTestState>(init, TestReducers.IdentitySimpleTestStateReducer);
+        SimpleTestState init = new(42);
+        var simpleStore = new Store<SimpleTestState>(init, TestReducers.IdentitySimpleTestStateReducer);
 
-            var subscriber = new EventCounterSubscriber<SimpleTestState>();
-            simpleStore.ObservableFor(state => state).Subscribe(subscriber);
+        var subscriber = new EventCounterSubscriber<SimpleTestState>();
+        simpleStore.ObservableFor(state => state).Subscribe(subscriber);
 
-            simpleStore.Dispatch(new EmptyAction());
+        simpleStore.Dispatch(new EmptyAction());
 
-            subscriber.Notified.Should().Be(0);
-        }
+        subscriber.Notified.Should().Be(0);
+    }
 
     [Fact]
     public void ObserverNotifiedOnDispatchWhenReducerDoesChangeState()
     {
-            SimpleTestState init = new(42);
-            var simpleStore = new Store<SimpleTestState>(init, TestReducers.IncrementNSimpleTestStateReducer);
+        SimpleTestState init = new(42);
+        var simpleStore = new Store<SimpleTestState>(init, TestReducers.IncrementNSimpleTestStateReducer);
 
-            var subscriber = new EventCounterSubscriber<SimpleTestState>();
-            simpleStore.ObservableFor(state => state).Subscribe(subscriber);
+        var subscriber = new EventCounterSubscriber<SimpleTestState>();
+        simpleStore.ObservableFor(state => state).Subscribe(subscriber);
 
-            simpleStore.Dispatch(new EmptyAction());
+        simpleStore.Dispatch(new EmptyAction());
 
-            subscriber.Notified.Should().Be(1);
-        }
+        subscriber.Notified.Should().Be(1);
+    }
 
 
     [Fact]
     public void ObserverNotNotifiedForChangeOutsideOfSelector()
     {
-            Point init = new(4, 2);
-            var pointStore = new Store<Point>(init, TestReducers.IncrementYPointReducer);
+        Point init = new(4, 2);
+        var pointStore = new Store<Point>(init, TestReducers.IncrementYPointReducer);
 
-            var subscriber = new EventCounterSubscriber<float>();
-            pointStore.ObservableFor(state => state.X).Subscribe(subscriber);
+        var subscriber = new EventCounterSubscriber<float>();
+        pointStore.ObservableFor(state => state.X).Subscribe(subscriber);
 
-            pointStore.Dispatch(new EmptyAction());
+        pointStore.Dispatch(new EmptyAction());
 
-            subscriber.Notified.Should().Be(0);
-        }
+        subscriber.Notified.Should().Be(0);
+    }
 
     [Fact]
     public void ObserverNotifiedForChangeInsideOfSelector()
     {
-            Point init = new(4, 2);
-            var pointStore = new Store<Point>(init, TestReducers.IncrementYPointReducer);
+        Point init = new(4, 2);
+        var pointStore = new Store<Point>(init, TestReducers.IncrementYPointReducer);
 
-            var subscriber = new EventCounterSubscriber<float>();
-            pointStore.ObservableFor(state => state.Y).Subscribe(subscriber);
+        var subscriber = new EventCounterSubscriber<float>();
+        pointStore.ObservableFor(state => state.Y).Subscribe(subscriber);
 
-            pointStore.Dispatch(new EmptyAction());
+        pointStore.Dispatch(new EmptyAction());
 
-            subscriber.Notified.Should().Be(1);
-        }
+        subscriber.Notified.Should().Be(1);
+    }
+
+    [Fact]
+    public void ErrorInSelectorDoesNotBreakOtherObservers()
+    {
+        SimpleTestState init = new(42);
+        var simpleStore = new Store<SimpleTestState>(init, TestReducers.IncrementNSimpleTestStateReducer);
+
+        int notified = 0;
+        int errors = 0;
+        simpleStore.ObservableFor(state => state)
+            .Subscribe(
+                onNext: _ => notified++,
+                onError: _ => errors++);
+        simpleStore.ObservableFor(TestSelectors.ErrorSimpleTestStateSelector)
+            .Subscribe(
+                onNext: _ => { },
+                onError: _ => { });
+
+        simpleStore.Dispatch(new EmptyAction());
+        simpleStore.Dispatch(new EmptyAction());
+
+        (notified, errors).Should().Be((2, 0));
+    }
 }
 
 internal class EventCounterSubscriber<T> : IObserver<T>
